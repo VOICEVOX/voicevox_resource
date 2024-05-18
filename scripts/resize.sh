ae9e39b2b7d8307186f5a7684901c9d861fa1fa0
Hiroshiba <hihokaruta@gmail.com>
2024-05-19 02:37:24 +0900

    追加変更

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -10,8 +10,10 @@ elif ! [ -d ./character_info ]; then
 fi
 
 # .png_large がなかったら、.png を .png_large にリネームしたあと 256x256 の .png を作成する
+# .png が 256x256 だった場合は何もしない
 for i in ./character_info/*/icon.png ./character_info/*/icons/*.png; do
   if [ -f "${i/%/_large}" ]; then continue; fi
+  if [ "$(magick identify -format "%w %h" "$i")" = "256 256" ]; then continue; fi
   echo "---"
   mv "$i" "${i/%/_large}"
   magick convert -resize 256x256 "${i/%/_large}" "$i"
