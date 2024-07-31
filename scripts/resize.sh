4be4affa253af50dcabb1d50f5812fe9a54fb53b
Hiroshiba <hihokaruta@gmail.com>
2024-08-01 01:19:52 +0900

    Revert "話者ごとにicon.pngを作る (#69)"
    This reverts commit b59b147b97eaa31cfdde7fc098076b2a6318cc70.

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -10,13 +10,11 @@ elif ! [ -d ./character_info ]; then
 fi
 
 # .png_large がなかったら、.png を .png_large にリネームしたあと 256x256 の .png を作成する
-# .png が 256x256 だった場合は何もしない
-for i in ./character_info/*/icon.png ./character_info/*/icons/*.png; do
+for i in ./character_info/*/icons/*.png; do
   if [ -f "${i/%/_large}" ]; then continue; fi
-  if [ "$(magick identify -format "%w %h" "$i")" = "256 256" ]; then continue; fi
   echo "---"
   mv "$i" "${i/%/_large}"
-  magick convert -resize 256x256 "${i/%/_large}" "$i"
+  convert -resize 256x256 "${i/%/_large}" "$i"
   file "${i%/*}"/*
 done
 echo "--->done!"
