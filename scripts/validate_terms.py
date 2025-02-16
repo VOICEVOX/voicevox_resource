0ca962f365d0efd0db63da9bab7b3b35cc059246
Hiroshiba <hihokaruta@gmail.com>
2025-02-16 16:03:22 +0900

    feat: 利用規約の検証機能を追加

--- scripts/validate_terms.py
+++ scripts/validate_terms.py
@@ -7,59 +7,8 @@ from dataclasses import dataclass
 from typing import Literal
 
 
-@dataclass
-class Term:
-    name: str
-    title: str
-    path: Path
-    type: Literal["markdown", "text"]
-
-    def __repr__(self):
-        return f"{self.name}"
-
-    def check_file_exist(self):
-        return self.path.is_file() and self.path.exists()
-
-    @property
-    def normalized_content(self) -> str:
-        with open(self.path, encoding="utf-8") as f:
-            content = f.read()
-
-        # markdown特有の記法を削除
-        if self.type == "markdown":
-            content = content.replace("  \n", "\n")  # "  \n"をただの改行へ
-            content = re.sub(r"\[|\]", "", content)  # リンクの[]を除去
-
-        # text用の記法を削除
-        elif self.type == "text":
-            content = re.sub(r"、\n", "、", content)  # 読みやすくするための改行を削除
-
-        # タイトル行を置き換え
-        content = re.sub(
-            rf"^# {self.title}", f"# 利用規約タイトル", content, flags=re.MULTILINE
-        )
-
-        return content
-
-
-class CoreTerm(Term):
-    @property
-    def normalized_content(self) -> str:
-        content = super().normalized_content
-
-        trim_string = (
-            "これは VOICEVOX コアライブラリです。\n"
-            "https://github.com/VOICEVOX/voicevox_core\n\n"
-            "---\n\n"
-        )
-        if trim_string in content:
-            content = content.split(trim_string, 1)[1]
-
-        return content
-
-
 def main():
-    terms = [
+    terms = (
         Term(
             name="engine-text",
             title="VOICEVOX エンジン利用規約",
@@ -84,19 +33,89 @@ def main():
             path=Path("vvm/README.txt"),
             type="text",
         ),
-    ]
+    )
     base_term = terms[0]
     target_terms = terms[1:]
 
     print("ファイルの存在確認")
     for term in terms:
-        assert term.check_file_exist(), f"{term.name} が存在しません"
+        term.validate_file_exist()
 
+    print("正規化の妥当性を確認")
+    for term in terms:
+        term.validate_normalized_content()
+
+    print("内容の比較")
     base_content = base_term.normalized_content
     for target_term in target_terms:
-        print(f"比較: {base_term} vs {target_term}")
+        print(f"{base_term} vs {target_term}")
         target_content = target_term.normalized_content
         compare_content(base_content, target_content)
+
+
+@dataclass
+class Term:
+    name: str
+    title: str
+    path: Path
+    type: Literal["markdown", "text"]
+
+    def __repr__(self):
+        return f"{self.name}"
+
+    def validate_file_exist(self):
+        assert self.path.is_file(), f"{self} がファイルではありません"
+        assert self.path.exists(), f"{self} が存在しません"
+
+    @property
+    def content(self) -> str:
+        with open(self.path, encoding="utf-8") as f:
+            return f.read()
+
+    @property
+    def normalized_content(self) -> str:
+        content = self.content
+
+        # markdown特有の記法を削除
+        if self.type == "markdown":
+            content = content.replace("  \n", "\n")  # "  \n"をただの改行へ
+            content = re.sub(r"\[|\]", "", content)  # リンクの[]を除去
+
+        # text用の記法を削除
+        elif self.type == "text":
+            content = re.sub(r"、\n", "、", content)  # 読みやすくするための改行を削除
+
+        # タイトル行を置き換え
+        content = re.sub(
+            rf"^# {self.title}", f"# 利用規約タイトル", content, flags=re.MULTILINE
+        )
+
+        return content
+
+    def validate_normalized_content(self):
+        # 正規化によって文字数が大きく変わらないことを確認
+        content = self.content
+        normalized_content = self.normalized_content
+        rate = len(normalized_content) / len(content)
+        assert (
+            0.9 < rate < 1.1
+        ), f"{self} の正規化により文字数が大きく変わっています: {rate}"
+
+
+class CoreTerm(Term):
+    @property
+    def normalized_content(self) -> str:
+        content = super().normalized_content
+
+        trim_string = (
+            "これは VOICEVOX コアライブラリです。\n"
+            "https://github.com/VOICEVOX/voicevox_core\n\n"
+            "---\n\n"
+        )
+        if trim_string in content:
+            content = content.split(trim_string, 1)[1]
+
+        return content
 
 
 def compare_content(base_content: str, target_content: str):
